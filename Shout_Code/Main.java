5ab87d7695eece66f0f6029d7c139b87ad980fc8
Collin-Varisco-UL <c00304484@louisiana.edu>
2022-09-02 16:21:45 -0500

    final

--- Shout_Code/Main.java
+++ Shout_Code/Main.java
@@ -5,12 +5,12 @@
     This code is my own.
  */
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 public class Main {
     public static int T; // Number of threads
     public static int S; // Number of times each thread should shout
-
 
     /* getT()
      * Collects user input for how many threads should be created.
@@ -47,12 +47,14 @@ public class Main {
         }
     }
 
+
+
     public static void main(String[] args) {
-        getT(); // User inputs threads
-        getS(); // User inputs shouts
+        // Problem 1
+        getT(); // User inputs the number of threads
+        getS(); // User inputs the number of shouts per thread
 
         Thread[] threads = new Thread[T];
-
         for (int i = 0; i < T; i++) {
             threads[i] = new Thread(new Shout(i));
         }
